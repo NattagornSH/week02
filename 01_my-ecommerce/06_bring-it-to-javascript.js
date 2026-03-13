29c2dc199918b12c9427defa5212f754a24b7f0e
Nattagorn <jay@MacBook-Air-khxng-Nattagorn.local>
2026-03-13 22:03:39 +0700

    commit for week02 assignments

---
+++ 01_my-ecommerce/06_bring-it-to-javascript.js
@@ -0,0 +1,40 @@
+let productList = ["product_id_1","product_id_2","product_id_3"]
+
+const Matcha ={
+    product_id: "product_id_1",
+    product_name: "Matcha green tea",
+    weigh: "100 g",
+    price: 100
+};
+
+const Chensa ={
+    product_id: "product_id_2",
+    product_name: "Chensa green tea",
+    weigh: "100 g",
+    price: 159
+};
+
+const Hojicha ={
+    product_id: "product_id_3",
+    product_name: "Hojicha green tea",
+    weigh: "100 g",
+    price: 299
+};
+
+let cart = [
+    {product_id:"product_id_1",
+        quantity: 100,
+    },
+    {product_id:"product_id_2",
+        quantity: 9,
+    },
+    { product_id:"product_id_3",
+        quantity: 1,
+    },
+]
+
+console.log(productList);
+console.log(Matcha);
+console.log(Chensa);
+console.log(Hojicha);
+console.log(cart);
